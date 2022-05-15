0dd65c2b739c4f6de3053f86c7fe2304dc9ad406
vasanthakumar <vasanthan297@gmail.com>
2022-05-15 14:39:46 +0800

    added

--- main.py
+++ main.py
@@ -1,32 +1,18 @@
-# import required packages
-# Create constructor object
-# Creating routing
-# Running web app
-
-
-from flask import Flask,render_template
-
+from flask import Flask, render_template
+from random import random
+from random import randint
 app = Flask(__name__)
-
-
 @app.route("/")
 def home():
-    return "Hello !!"
+        bg_color = "black"
+        font_color = "white"
+        msg = "Hello"
+        value = randint(0, 3)
+        names = ['Logic will get you from A to B. Imagination will take you everywhere.', 'There are 10 kinds of people. Those who know binary and those who dont', 'There are two ways of constructing a software design. One way is to make it so simple that there are obviously no deficiencies and the other is to make it so complicated that there are no obvious deficiencies', 'It is pitch dark. You are likely to be eaten by a grue','It is not that is so smart,it is just that I stay with problems longer']
+      
+        return render_template('index.html', bg_color=bg_color, font_color=font_color, msg=names[value])
 
-@app.route("/<string:username>")
-def name(username):
-    return "<html><h1> Welcome {} </h1></html>".format(username)
-
-@app.route("/html_page")
-def html():
-    return render_template("demo.html")
-
-@app.route("/css")  
-def css():
-    return render_template("css.html")
-
-if __name__ == "__main__":
-    app.run(port=8080)
-
-
-
+if __name__ == '__main__':
+       app.run(host="0.0.0.0")
+    #margin-top: -100px; 
+    #margin-left: -900px; 
